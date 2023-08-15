c9f38312ac517a69e0e23632a85c839879073271
Ankita Pathak <iamankipathak@gmail.com>
2023-08-15 16:21:58 +0530

    commit 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rw = new RWOnly();
+//    rw.name = "Ankita";
+    // name has private access in com.driver.RWOnly
+    rw.setName("anki");
+    rw.getName();
+
+    System.out.println(rw.getName());
+
+  }
 }
